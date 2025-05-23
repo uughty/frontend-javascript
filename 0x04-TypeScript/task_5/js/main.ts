5232702cfb30773857d3b6acc2335d03a6902fcb
Malilo <sandramalilo21@gmail.com>
2025-05-23 08:10:43 +0300

    Task 5: Add nominal typing example with sum functions in main.ts

---
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -0,0 +1,29 @@
+export interface MajorCredits {
+  credits: number;
+  brand: 'major';
+}
+
+export interface MinorCredits {
+  credits: number;
+  brand: 'minor';
+}
+
+export function sumMajorCredits(
+  subject1: MajorCredits,
+  subject2: MajorCredits
+): MajorCredits {
+  return {
+    credits: subject1.credits + subject2.credits,
+    brand: 'major',
+  };
+}
+
+export function sumMinorCredits(
+  subject1: MinorCredits,
+  subject2: MinorCredits
+): MinorCredits {
+  return {
+    credits: subject1.credits + subject2.credits,
+    brand: 'minor',
+  };
+}
